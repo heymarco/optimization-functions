ada3cb2a9b9900039e85a056f3dc1cdf3f17da20
Marco Heyden <marco.heyden@kit.edu>
2021-11-04 09:43:06 +0100

    export OptimizationFunction

--- optifn/__init__.py
+++ optifn/__init__.py
@@ -1,5 +1,6 @@
 
 __version__ = "0.0.1"
 
+from .abstract import OptimizationFunction
 from .functions import *
 from .callables import *
--- optifn/abstract.py
+++ optifn/abstract.py
@@ -4,7 +4,6 @@ from typing import List
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cm
-import seaborn as sns
 from mpl_toolkits.mplot3d import Axes3D
 
 
